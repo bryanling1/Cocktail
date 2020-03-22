b46d7f8a817c8b020c0fe5c40ad36f8db87e6c50
b3ling <b3ling@edu.uwaterloo.ca>
2020-03-22 18:50:56 -0400

    Added push notification for empty drinks

--- timer.js
+++ timer.js
@@ -85,7 +85,12 @@ function pushSuccessNotification() {
       Notification.requestPermission().then(function (permission) {
         // If the user accepts, let's create a notification
         if (permission === "granted") {
-          var notification = new Notification("Your Cocktail is Ready!");
+            var notification = new Notification("Your Cocktail is Ready!", 
+            {
+                "icon": "./images/default.png",
+                "body": "Enjoy!"
+            }
+             );
         }
       });
     }
@@ -93,6 +98,43 @@ function pushSuccessNotification() {
     // At last, if the user has denied notifications, and you 
     // want to be respectful there is no need to bother them any more.
   }
+
+  function pushEmptyNotification() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+  
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification("You're out of cocktails!", 
+      {
+          "icon": "./images/default.png",
+          "body": "Get back on it!"
+      }
+       );
+    }
+  
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+            var notification = new Notification("You're out of cocktails!", 
+            {
+                "icon": "./images/default.png",
+                "body": "Get back on it!"
+            }
+            );
+        }
+      });
+    }
+  
+    // At last, if the user has denied notifications, and you 
+    // want to be respectful there is no need to bother them any more.
+  }
+
 
 chrome.extension.onMessage.addListener(function(req, sender, sendResponse) {
     if(req.message == "startTimer"){
@@ -127,7 +169,7 @@ if(video){
     });
     pauseVideo = function(){
         video.pause();
-        alert("You're out of drinks!")
+        pushEmptyNotification();
     }
 }
 
