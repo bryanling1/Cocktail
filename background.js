f670e0666e548a3348cb0079f0b409454d08c97c
b3ling <b3ling@edu.uwaterloo.ca>
2020-03-22 12:28:08 -0400

    cocktail time saves with storage api

--- background.js
+++ background.js
@@ -9,18 +9,26 @@ chrome.runtime.onInstalled.addListener(function() {
 
 let timerInterval = null;
 let timerVal = null;
+let initTimerVal = 120;
 let isTimerOn = false;
 
-chrome.extension.onMessage.addListener(function(req, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
     if(req.message == "timerButton"){
         if(isTimerOn == true){
             isTimerOn = false;
             stopClock(timerInterval);
         }else{
-            timerVal = timerVal?(timerVal):(req.time);
+            initTimerVal = req.time;
+            timerVal = timerVal?(timerVal):(initTimerVal);
             isTimerOn = true;
             timerInterval = setInterval(clockDown, 1000);
         }
+    }else if(req.message == "showTime"){
+        sendClockVal(timerVal?(timerVal):(initTimerVal));
+    }else if(req.message == "getCockTime"){
+        chrome.storage.sync.get("cocktailSeconds", function(data){
+            chrome.extension.sendMessage({"message": "displayCocktailTime", "data": data["cocktailSeconds"]})
+        });
     }
 })
 
@@ -29,14 +37,26 @@ function stopClock(){
 }
 
 function clockDown(){
-    sendClockVal(timerVal);
+    if(timerVal <= 1){
+        stopClock();
+        success();
+    }
     timerVal -= 1;
+    sendClockVal(timerVal);
 }
 
 function sendClockVal(val){
     chrome.extension.sendMessage({"message": "startTimer", "time": val});
 }
 
+function success(){
+    chrome.storage.sync.get("cocktailSeconds", function(data){
+        chrome.storage.sync.set(
+            {cocktailSeconds: data["cocktailSeconds"] + initTimerVal,
+        });
+        chrome.extension.sendMessage({"message": "displayCocktailTime", "data": data["cocktailSeconds"] + initTimerVal})
+    });
+}
 
 
 
--- timer.js
+++ timer.js
@@ -1,13 +1,14 @@
 
 const playButtonRef = document.getElementById('timerToggle');
 const clockRef = document.getElementById('clock');
+const cockTimeRef = document.getElementById('cockTime');
 let timerInterval = null;
 let timerVal = 10;
 let isTimerOn = false;
 
-playButtonRef.onclick = function(){
+playButtonRef ? (playButtonRef.onclick = function(){
     timerButton();
-}
+}):(null);
 
 function timerButton(){
     chrome.extension.sendMessage({"message":"timerButton", "time": timerVal});
@@ -17,10 +18,26 @@ function displayTime(data){
     clockRef.innerHTML = data;
 }
 
+function getCurrentTime(){
+    chrome.extension.sendMessage({"message":"showTime"});
+}
+
+function displayCockTime(data){
+    cockTimeRef.innerHTML = data;
+}
+
+function getCockTime(){
+    chrome.extension.sendMessage({"message":"getCockTime"});
+}
 
 chrome.extension.onMessage.addListener(function(req, sender, sendResponse) {
     if(req.message == "startTimer"){
         displayTime(req.time);
+    }else if(req.message == "displayCocktailTime"){
+        displayCockTime(req.data);
     }
 })
+
+getCurrentTime();
+getCockTime();
 
