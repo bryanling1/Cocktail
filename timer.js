22b1587aad627f4dd03e00419b232a91c508d569
b3ling <b3ling@edu.uwaterloo.ca>
2020-03-22 19:50:17 -0400

    moved success notification to background script

--- timer.js
+++ timer.js
@@ -9,8 +9,8 @@ let isVideoPlaying = false;
 let cocktails = [
     {
         "name": "first",
-        "cocktailSeconds": 10,
-        "timeUntilReady": 10
+        "cocktailSeconds": 0,
+        "timeUntilReady": 5
     }
 ]
 let cocktailsIndex = 0;
@@ -63,41 +63,7 @@ function secondsToClockString(time){
     let secondsDisplay = seconds < 10?("0" + seconds):(seconds);
     return minutes + ":" +  secondsDisplay;
 }
-function pushSuccessNotification() {
-    // Let's check if the browser supports notifications
-    if (!("Notification" in window)) {
-      alert("This browser does not support desktop notification");
-    }
-  
-    // Let's check whether notification permissions have already been granted
-    else if (Notification.permission === "granted") {
-      // If it's okay let's create a notification
-      var notification = new Notification("Your Cocktail is Ready!", 
-      {
-          "icon": "./images/default.png",
-          "body": "Enjoy!"
-      }
-       );
-    }
-  
-    // Otherwise, we need to ask the user for permission
-    else if (Notification.permission !== "denied") {
-      Notification.requestPermission().then(function (permission) {
-        // If the user accepts, let's create a notification
-        if (permission === "granted") {
-            var notification = new Notification("Your Cocktail is Ready!", 
-            {
-                "icon": "./images/default.png",
-                "body": "Enjoy!"
-            }
-             );
-        }
-      });
-    }
-  
-    // At last, if the user has denied notifications, and you 
-    // want to be respectful there is no need to bother them any more.
-  }
+
 
   function pushEmptyNotification() {
     // Let's check if the browser supports notifications
@@ -147,7 +113,6 @@ chrome.extension.onMessage.addListener(function(req, sender, sendResponse) {
         pauseVideo();
     }else if (req.message == "success"){
         toggleSuccess();
-        pushSuccessNotification();
     }
 })
 
